0279cc0d56040faf7e616dd79245c43e0acc2655
noumansharif27 <noumansharifgul27@gmail.com>
2025-11-17 17:48:22 +0500

    working on an error on page rendering

--- frontEnd/src/components/LatestCollections.jsx
+++ frontEnd/src/components/LatestCollections.jsx
@@ -1,10 +1,43 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
+import Tittle from "./Tittle";
+import ProductItem from "./ProductItem";
 
 const LatestCollections = () => {
   const { products } = useContext(ShopContext);
-  console.log(products);
-  return <div></div>;
+
+  let [latestProducts, setLatestProducts] = useState();
+
+  useEffect(() => {
+    setLatestProducts(products.slice(0, 10));
+  }, []);
+
+  return (
+    <div className="my-10">
+      <div className="text-center -py-8 text-3xl">
+        <Tittle text1={"LATEST"} text2={"COLLECTIONS"} />
+        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
+          dignissimos ipsam nihil, sed illum atque laborum eos vel ad ducimus
+          corrupti hic quia est vitae adipisci minima perspiciatis magnam
+          voluptas, saepe error! Odio, tenetur fuga? Ducimus, non. Tenetur,
+          officia sunt!
+        </p>
+      </div>
+
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {latestProducts.map((product, index) => (
+          <ProductItem
+            key={index}
+            id={product._id}
+            image={product.image}
+            name={product.name}
+            price={product.price}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default LatestCollections;
