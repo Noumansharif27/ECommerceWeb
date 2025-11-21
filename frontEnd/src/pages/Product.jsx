b42d8b92aff1cf0c538049c4183d9d91e82ce1f1
NoumanSharif27 <noumansharifgul27@gmail.com>
2025-11-21 23:12:57 +0500

    working on reviews

--- frontEnd/src/pages/Product.jsx
+++ frontEnd/src/pages/Product.jsx
@@ -8,6 +8,7 @@ const Product = () => {
   const { products, currency } = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
   const [image, setImage] = useState("");
+  const [size, setSize] = useState("");
 
   const fetchProductData = async () => {
     products.map((item) => {
@@ -29,7 +30,7 @@ const Product = () => {
       <div className="flex gap 12 sm:gap-12 flex-col sm:flex-row">
         {/* Product Images */}
         <div className="flex-1 flex flex-col-reverse gap-3 sm:flex-row">
-          <div className="flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full">
+          <div className="flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full product-images">
             {productData.image.map((item, index) => (
               <img
                 onClick={() => setImage(item)}
@@ -59,6 +60,59 @@ const Product = () => {
           <p className="mt-5 text-3xl font-medium">
             {currency}
             {productData.price}
+          </p>
+          <p className="mt-5 text-gray-500 md:w-4/5">
+            {productData.description}
+          </p>
+          <div className="flex flex-col gap-4 my-8">
+            <p>Select size</p>
+            <div className="flex gap-2">
+              {productData.sizes.map((item, index) => (
+                <button
+                  onClick={() => setSize(item)}
+                  key={index}
+                  className={`border py-2 px-4 bg-gray-100 cursor-pointer ${
+                    item == size ? `border-orange-500` : ""
+                  }`}
+                >
+                  {item}
+                </button>
+              ))}
+            </div>
+          </div>
+          <button className="bg-black text-white px-8 py-3 text-sm active:bg-gray-600 cursor-pointer">
+            ADD TO CART
+          </button>
+          <hr className="mt-8 sm:w-4/5 border-gray-300" />
+          <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1">
+            <p>100% Original product.</p>
+            <p>Cash on delivery is avaliable on this product.</p>
+            <p>Easy return and exchange policy within 7 days</p>
+          </div>
+        </div>
+      </div>
+
+      {/* Description & Review Section */}
+      <div className="mt-20">
+        <div className="flex">
+          <b className="border px-5 py-3 text-sm">Description</b>
+          <p className="border px-5 py-3 text-sm">Reviews [112]</p>
+        </div>
+        <div className="flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure,
+            doloremque culpa architecto corrupti distinctio autem qui rem eius
+            accusamus asperiores! Velit sint accusamus ducimus praesentium
+            dolorem? Ipsa aperiam reiciendis magni distinctio rem alias!
+            Similique, quisquam?
+          </p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+            iste ducimus porro tenetur doloremque. Molestias sit quis
+            repellendus facere totam aspernatur, quaerat explicabo! Cumque rerum
+            quisquam, error reiciendis eos necessitatibus fuga laudantium harum
+            quo voluptate magni consequatur nulla quod adipisci esse ullam
+            voluptas. Corrupti, ipsam.
           </p>
         </div>
       </div>
