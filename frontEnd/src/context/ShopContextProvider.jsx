f876a0a61fddb1672f0b0df381a957b91a69a4ff
noumansharif27 <noumansharifgul27@gmail.com>
2025-11-22 12:15:22 +0500

    added functionality of addToCart

--- frontEnd/src/context/ShopContextProvider.jsx
+++ frontEnd/src/context/ShopContextProvider.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { products } from "../assets/assets";
 import { ShopContext } from "./ShopContext";
 
@@ -7,6 +7,26 @@ const ShopContextProvider = (props) => {
   const delivery_fee = 170;
   const [search, setSearch] = useState("");
   const [showSearch, setShowSearch] = useState(false);
+  const [cartItems, setCartItems] = useState({});
+
+  const addToCart = async (itemId, size) => {
+    let cartData = structuredClone(cartItems);
+    if (cartData[itemId]) {
+      if (cartData[itemId][size]) {
+        cartData[itemId][size] += 1;
+      } else {
+        cartData[itemId][size] = 1;
+      }
+    } else {
+      cartData[itemId] = {};
+      cartData[itemId][size] = 1;
+    }
+    setCartItems(cartData);
+  };
+
+  useEffect(() => {
+    console.log(cartItems);
+  }, [cartItems]);
 
   const value = {
     products,
@@ -16,6 +36,8 @@ const ShopContextProvider = (props) => {
     setSearch,
     showSearch,
     setShowSearch,
+    cartItems,
+    addToCart,
   };
 
   return (
