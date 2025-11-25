a8c53366a47b8c9febbfd53b31cdf66804e0a24e
noumansharif27 <noumansharifgul27@gmail.com>
2025-11-25 16:18:17 +0500

    working on deleting the product from listing

--- frontEnd/src/pages/Cart.jsx
+++ frontEnd/src/pages/Cart.jsx
@@ -1,9 +1,82 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Tittle from "../components/Tittle";
+import { assets } from "../assets/assets";
 
 const Cart = () => {
+  const { products, currency, cartItems } = useContext(ShopContext);
+  const [cartData, setCartData] = useState([]);
+
+  useEffect(() => {
+    const tempData = [];
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        if (cartItems[items][item] > 0) {
+          tempData.push({
+            _id: items,
+            size: item,
+            quantity: cartItems[items][item],
+          });
+        }
+      }
+    }
+    setCartData(tempData);
+  }, [cartItems]);
   return (
-    <div>
-      <h1>Cart</h1>
+    <div className="border-t pt-14">
+      <div className="text-2xl mb-3">
+        <Tittle text1={"YOUR"} text2={"CART"} />
+      </div>
+
+      <div>
+        {cartData.map((item, index) => {
+          const productData = products.find(
+            (product) => product._id === item._id
+          );
+
+          return (
+            <div
+              key={index}
+              className="py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4"
+            >
+              <div className="flex items-start gap-6">
+                <img
+                  src={productData.image[0]}
+                  className="w-16 sm:w-20"
+                  alt="Product_Image"
+                />
+                <div>
+                  <p className="text-xs sm:text-lg font-medium">
+                    {productData.name}
+                  </p>
+                  <div className="flex items-center mt-2 gap-5">
+                    <p>
+                      {currency}
+                      {productData.price}
+                    </p>
+                    <p className="px-2 s:-px-3 sm:py-1 border  bg-slate-50">
+                      {item.size}
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <input
+                type="number"
+                name=""
+                id=""
+                min={1}
+                defaultValue={item.quantity}
+                className="border mx-w-10 sm:mx-w-20 px-1 sm:px-2 py-1"
+              />
+              <img
+                src={assets.bin_icon}
+                alt="Delete_Icon"
+                className="w-4 mr-4 sm:w-5 cursor-pointer"
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
