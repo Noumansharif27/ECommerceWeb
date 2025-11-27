00854b81db54efcacb049e20af74d481754c665a
noumansharif27 <noumansharifgul27@gmail.com>
2025-11-27 11:44:02 +0500

    working on login style

--- frontEnd/src/components/Navbar.jsx
+++ frontEnd/src/components/Navbar.jsx
@@ -43,11 +43,13 @@ export default function Navbar() {
         />
 
         <div className="group relative">
-          <img
-            className="w-5 cursor-pointer"
-            src={assets.profile_icon}
-            alt="NavBAr_ProfileIcon"
-          />
+          <Link to="/login">
+            <img
+              className="w-5 cursor-pointer"
+              src={assets.profile_icon}
+              alt="NavBAr_ProfileIcon"
+            />
+          </Link>
 
           <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
             <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
