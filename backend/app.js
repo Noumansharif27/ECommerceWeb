d06d012c31c7d7d816ab2c0b7eb6e3490319b14b
noumansharif27 <noumansharifgul27@gmail.com>
2025-11-29 11:04:24 +0500

    working on backend

---
+++ backend/app.js
@@ -0,0 +1,40 @@
+// requiring the .env file and using it once in production
+if (process.env_NODE_ENV != "production") {
+  require("dotenv").config();
+}
+
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const { ConnectCloudinary } = require("./config/cloudinary.js");
+
+const { ConnectDB } = require("./config/cloudinary.js");
+
+const app = express();
+const PORT = process.env.SERVER_PORT || 4000;
+const dbUrl = process.env.DB_URL;
+
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((ERR) => {
+    console.log(ERR);
+  });
+
+async function main() {
+  await mongoose.connect(dbUrl);
+}
+ConnectCloudinary();
+
+// middleware
+app.use(express.json()); // it will parse data into json
+app.use(cors()); // it will help to access backend from any ID
+
+app.get("/", (req, res) => {
+  res.send("Server is Started");
+});
+
+app.listen(PORT, () => {
+  console.log(`App is Listening at PORT: ${PORT}`);
+});
