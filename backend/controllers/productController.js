8a4035377e88fcc738f64cd8ab9cdd6231daff6c
noumansharif27 <noumansharifgul27@gmail.com>
2025-12-05 12:46:47 +0500

    successfully added the images to cloud

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,5 @@
+import { v2 as cloudinary } from "cloudinary";
+
 // Add product
 const addProduct = async (req, res) => {
   try {
@@ -16,6 +18,10 @@ const addProduct = async (req, res) => {
     const image3 = req.files.image3 && req.files.image3[0];
     const image4 = req.files.image4 && req.files.image4[0];
 
+    const images = [image1, image2, image3, image4].filter(
+      (image) => image !== undefined
+    );
+
     console.log(
       name,
       description,
@@ -26,7 +32,17 @@ const addProduct = async (req, res) => {
       bestSeller
     );
 
-    console.log(image1, image2, image3, image4);
+    // uploading the images to cloudinary.
+    let imageUrl = await Promise.all(
+      images.map(async (item) => {
+        let result = await cloudinary.uploader.upload(item.path, {
+          resource_type: "image",
+        });
+        return result.secure_url;
+      })
+    );
+    console.log(imageUrl);
+
     res.json({});
   } catch (error) {
     console.log(error);
