cba830187929653841fdde5380a32bce80fa4130
NoumanSharif27 <noumansharifgul27@gmail.com>
2025-12-07 23:52:46 +0500

    workin on list route

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import { backendUrl } from "../App";
 
 const List = () => {
+  const [list, setList] = useState([]);
+
+  const fetchList = async () => {
+    try {
+      const response = await axios.get(backendUrl + "/api/product/list");
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchList();
+  }, []);
   return <div></div>;
 };
 
