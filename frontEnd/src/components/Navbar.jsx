e6b6f3b524a4d2b9e433c59fc65f31581276c512
NoumanSharif27 <noumansharifgul27@gmail.com>
2025-12-16 21:28:57 +0500

    solving the tinny details

--- frontEnd/src/components/Navbar.jsx
+++ frontEnd/src/components/Navbar.jsx
@@ -60,21 +60,25 @@ export default function Navbar() {
         <div className="group relative">
           <Link to="/login">
             <img
+              onClick={() => (token ? null : navigate("/login"))}
               className="w-5 cursor-pointer"
               src={assets.profile_icon}
               alt="NavBAr_ProfileIcon"
             />
           </Link>
 
-          <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
-            <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
-              <p className="cursor-pointer hover:text-black">My Profile</p>
-              <p className="cursor-pointer hover:text-black">Orders</p>
-              <p onClick={logout} className="cursor-pointer hover:text-black">
-                Logout
-              </p>
+          {/* dropDwn Menu */}
+          {token && (
+            <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
+              <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
+                <p className="cursor-pointer hover:text-black">My Profile</p>
+                <p className="cursor-pointer hover:text-black">Orders</p>
+                <p onClick={logout} className="cursor-pointer hover:text-black">
+                  Logout
+                </p>
+              </div>
             </div>
-          </div>
+          )}
         </div>
         <Link to="/cart" className="relative">
           <img
