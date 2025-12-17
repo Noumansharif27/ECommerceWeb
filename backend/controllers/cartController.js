bc2b0e089101a97f0cf84e3fce871118772a21e7
noumansharif27 <noumansharifgul27@gmail.com>
2025-12-17 12:03:37 +0500

    working on the compay laptop for improvement

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -2,21 +2,24 @@ import userModel from "../models/user.js";
 
 // Add to cart
 const addCart = async () => {
-    try {
-        const { userId, itemId, size } = req.body;
-        const userData = await userModel.findById(userId);
-        let cartData = await userData.cartData;
-        if (cartData[itemId]) {
-            if (cartData[itemId][size]) {
-                cartData[itemId][size] += 1;
-            } else {
-                cartData[itemId][size] = 1;
-            }
-        } else {
-            cartData[itemId] = {}
-            cartData[itemId][size] = 1
-        }
+  try {
+    const { userId, itemId, size } = req.body;
+    const userData = await userModel.findById(userId);
+    let cartData = await userData.cartData;
+    if (cartData[itemId]) {
+      if (cartData[itemId][size]) {
+        cartData[itemId][size] += 1;
+      } else {
+        cartData[itemId][size] = 1;
+      }
+    } else {
+      cartData[itemId] = {};
+      cartData[itemId][size] = 1;
     }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
 };
 
 // Update cart
