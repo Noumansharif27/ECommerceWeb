44a7fed467d6a75a9cd99b918d55893c2037b4b6
NoumanSharif27 <noumansharifgul27@gmail.com>
2025-12-21 22:54:23 +0500

    workin on the related products grid style

--- frontEnd/src/components/RelatedPrduct.jsx
+++ frontEnd/src/components/RelatedPrduct.jsx
@@ -96,7 +96,7 @@ const RelatedPrduct = ({ category, subCategory }) => {
         <Tittle text1={"RELATED"} text2={"ITEM"} />
       </div>
 
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+      <div className="grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-4 gap-y-6">
         {related.map((product, index) => (
           <ProductItem
             key={index}
