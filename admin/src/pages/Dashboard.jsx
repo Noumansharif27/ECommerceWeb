8a0231c4406e3c6c2572243fe71b844ce2a03999
noumansharif27 <noumansharifgul27@gmail.com>
2026-01-02 12:51:55 +0500

    working on the admin panel dashborad

--- admin/src/pages/Dashboard.jsx
+++ admin/src/pages/Dashboard.jsx
@@ -1,26 +1,44 @@
 import React from "react";
 
 const Dashboard = () => {
-    return (
-        <div px-5>
-            <div className="top">
-                <div className="dashboard-nav w-full bg-red-500 flex justify-between">
-                    <div className="divs min-w-[20%] bg-blue-500 rounded p-1">
-                        <p>Lorem ipsum dolor sit amet.
-                        </p>
-                    </div>
-                     <div className="divs w-[20%] bg-blue-500 rounded">
-                        <p>Lorem ipsum dolor sit amet.
-                        </p>
-                    </div> <div className="divs w-[20%] bg-blue-500 rounded">
-                        <p>Lorem ipsum dolor sit amet.
-                        </p>
-                    </div>
-                </div>
+  return (
+    <div px-5>
+      <div className="top">
+        <div className="dashboard-nav w-full flex justify-between">
+          <div className="divs min-w-[30%] bg-slate-100 rounded-md p-3">
+            <p className="text-gray-400 text-sm">Weekly Sale</p>
+            <b className="text-md">$12,233</b>
+            <hr className="text-gray-300 my-2" />
+            <div className="flex gap-2">
+              <span className="text-sm text-green-500">+60%</span>
+              <span className="text-sm text-gray-400">Then last week</span>
             </div>
-            <div className="bottom"></div>
+          </div>
+
+          <div className="divs min-w-[30%] bg-slate-100 rounded-md p-3">
+            <p className="text-gray-400 text-sm">Weekly Sale</p>
+            <b className="text-sm">$12,233</b>
+            <hr className="text-gray-300 my-2" />
+            <div className="flex gap-2">
+              <span className="text-sm text-green-500">+60%</span>
+              <span className="text-sm text-gray-400">Then last week</span>
+            </div>
+          </div>
+
+          <div className="divs min-w-[30%] bg-slate-100 rounded-md p-3">
+            <p className="text-gray-400 text-sm">Weekly Sale</p>
+            <b className="text-sm">$12,233</b>
+            <hr className="text-gray-300 my-2" />
+            <div className="flex gap-2">
+              <span className="text-sm text-green-500">+60%</span>
+              <span className="text-sm text-gray-400">Then last week</span>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+      <div className="bottom"></div>
+    </div>
+  );
 };
 
 export default Dashboard;
